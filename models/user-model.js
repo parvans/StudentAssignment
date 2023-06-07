c2d7b5a4143a3e0f8ed32948535804b2611b7120
parvans <parvansajeevan@gmail.com>
2023-06-07 16:47:05 +0530

    user model created

---
+++ models/user-model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+export const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength:8
+    },
+  },{
+    timestamps:true
+  })
+);
