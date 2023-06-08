e1023d1aa2112173adb6518b61cc1c6d743bac87
parvans <parvansajeevan@gmail.com>
2023-06-08 12:22:34 +0530

    homework submit completed

--- controllers/homework-controller.js
+++ controllers/homework-controller.js
@@ -29,18 +29,30 @@ export const attentAssignment = async (req, res) => {
         })
     })
     const result=await newHomeWork.save();
-    
-    // result.answers.map(async (answer)=>{
-    //     const question=assignment.questions.find(question=>question.
-    //     if(question.answer===answer.answer){
-    //         const updatedHomeWork=await HomeWork.findByIdAndUpdate(result._id,{
-    //             $inc:{totalMark:question.mark}
-    //         },{new:true});
-    //     }
-    //     console.log(question);
-    // })
 
-   
+    result.answers.map(async (answer)=>{
+        const update=await HomeWork.findByIdAndUpdate(result._id,{
+            $set:{
+                [`answers.${answer.questNo-1}.answer`]:req.body.answers[answer.questNo-1].answer
+            }
+        },{new:true});
+
+    })
+
+    const updatedHomeWork=await HomeWork.findById(result._id);
+    updatedHomeWork.answers.map(async (answer)=>{
+        const question=assignment.questions.find(question=>question.questionNo===answer.questNo);
+        if(question.answer===answer.answer){
+            const updatedHomeWork=await HomeWork.findByIdAndUpdate(result._id,{
+                $inc:{totalMark:question.mark}
+            },{new:true});
+        }
+    })
+    
+    const updateAssign=await Assignment.findByIdAndUpdate(req.params.id,{
+        $push:{attendedStudents:result._id}
+    },{new:true});
+
     
     res.status(200).json({message:"Assignment submitted successfully"});
 
