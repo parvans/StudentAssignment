0b7224af49a4e87ceeada89398735a567cfe1acc
parvans <parvansajeevan@gmail.com>
2023-06-09 10:38:59 +0530

    added allstudents route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { userLogin, userRegister } from "../controllers/user-controller.js";
+import { allStudents, userLogin, userRegister } from "../controllers/user-controller.js";
 import faculty from "../middleware/faculty.js";
 import auth from "../middleware/auth.js";
 const router = express.Router();
 router.post("/register",[faculty,auth],userRegister);
 router.post("/login",userLogin);
-
+router.get('/allstudents',allStudents);
 export default router;
