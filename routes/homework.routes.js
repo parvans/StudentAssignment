a3bb04b33480cdfd499a8242e0e8d7abddb37514
parvans <parvansajeevan@gmail.com>
2023-06-14 10:22:44 +0530

    getallstuass modified 😁

--- routes/homework.routes.js
+++ routes/homework.routes.js
@@ -4,7 +4,7 @@ import auth from "../middleware/auth.js";
 import { attentAssignment, getAStudentHomeWork, getAllHomeWork, getAllStuPdf, getStuAttHomeWork, getStuPertiAttHomeWork } from "../controllers/homework-controller.js";
 const router = express.Router();
 router.post("/getq/:id",auth,attentAssignment);
-router.get("/getallstuass",getAllHomeWork);
+router.get("/getallstuass",[faculty,auth],getAllHomeWork);
 router.get("/getastuass/:id",[faculty,auth],getAStudentHomeWork);
 router.get("/getastuattass",auth,getStuAttHomeWork)
 router.get("/getastupertiattass/:id",auth,getStuPertiAttHomeWork)
